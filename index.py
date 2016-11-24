7c64a5bcc4b8d1d2089dd11427986ff726991771
egusa hiroaki <egusa.hiroaki@gmail.com>
2016-11-25 00:58:23 +0900

    figを作成/保存

--- index.py
+++ index.py
@@ -6,6 +6,10 @@ import time
 from flask import Flask, render_template, request, redirect, url_for
 import csv
 import os
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import stats
+
 
 app = Flask(__name__)
 
@@ -31,6 +35,34 @@ def analysis():
       writer.writerows(csv.reader(csv_lines, delimiter=",", quotechar='"'))
 
     # analysis
+    csv_data = np.genfromtxt('csv/upload.csv', delimiter=",", names=True, dtype=np.uint8)
+    column_names = csv_data.dtype.names # get header column name
+
+    # this conversion may be not needed.
+    data = np.array(csv_data.tolist())
+    x = data[:, 0] # get 0 row
+    y = data[:, 1]
+
+    plt.rc('font', **{'family': 'serif'})
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    slope, intercept, r, _, _ = stats.linregress(x, y)
+
+    print("slope: ", slope)
+    print("intercept: ", intercept)
+    print("r: ", r)
+
+    plt.xlabel(column_names[0]) # x 
+    plt.ylabel(column_names[1]) # y 
+    sc = ax.scatter(x, y, s=25, marker='x', color='b')
+    plt.plot(x, x*slope + intercept)
+    # グリッド表示
+    ax.grid(True)
+    local_path = "fig.png"
+    plt.savefig(local_path)
+    plt.show()
+    plt.close("all")
 
     return render_template('index.html')
 
