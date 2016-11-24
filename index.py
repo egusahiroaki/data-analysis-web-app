21a7e0c6dd99c417d575d99f6539fcacafda0448
egusa hiroaki <egusa.hiroaki@gmail.com>
2016-11-24 23:53:57 +0900

    ローカルにcsv保存

--- index.py
+++ index.py
@@ -4,6 +4,8 @@
 from __future__ import print_function
 import time
 from flask import Flask, render_template, request, redirect, url_for
+import csv
+import os
 
 app = Flask(__name__)
 
@@ -11,6 +13,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/analysis', methods=['GET', 'POST'])
+def analysis():
+    file = request.files['file'] 
+    csv_lines = []
+
+    for line in file.stream.readlines():
+        csv_lines.append(line.decode('utf-8'))
+
+    # if there is no directory, then create
+    if not os.path.isdir("csv"):
+      os.makedirs("csv")
+
+    # save csv in local directory
+    with open('csv/upload.csv', 'w') as f:
+      writer = csv.writer(f, lineterminator='\n')
+      writer.writerow(csv_lines)
+
+    # 
+
+    return render_template('index.html')
+
 if __name__ == "__main__":
-    print('on index')
+    app.debug = True # デバッグモード有効化
     app.run(host="127.0.0.1", port=5000)
